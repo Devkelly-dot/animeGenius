e99a099b9fe5a4de6277a4fef7f274e2ddfce8c3
2toesxtra <devkelly9622@gmail.com>
2024-06-13 14:39:56 -0400

    fixed top anime controller import

--- controllers/anime/fetchPopularController.js
+++ controllers/anime/fetchPopularController.js
@@ -1,12 +1,12 @@
 const { FetchPopularAnimeSerializer } = require("../../serializers/action/anime/fetchPopular");
-const { PopularAnimeSerializer } = require("../../serializers/return/anime/rapidApiAnimeSerializer");
+const { RapidApiAnimeSerializer } = require("../../serializers/return/anime/rapidApiAnimeSerializer");
 const { BaseController } = require("../base/base");
 
 class FetchPopoularController extends BaseController {
     constructor() {
         super();
         this.actionSerializer = FetchPopularAnimeSerializer;
-        this.returnSerializer = PopularAnimeSerializer;
+        this.returnSerializer = RapidApiAnimeSerializer;
     }
 }
 
